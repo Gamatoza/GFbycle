1fd878d66e5bf617ddd9c5124a4d8894bdeac5a1
Gamatoza TV <gamatext@yandex.ru>
2020-04-19 00:20:12 +0300

    data update

---
+++ DBReq/dataUpdate190420.sql
@@ -0,0 +1,156 @@
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: TestDB
+-- ------------------------------------------------------
+-- Server version	5.7.29-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Forms`
+--
+
+DROP TABLE IF EXISTS `Forms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Forms` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `TypeID` int(11) DEFAULT NULL,
+  `QuestionsID` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `CorrectAnswer` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fkToTypes` (`TypeID`),
+  CONSTRAINT `fkToTypes` FOREIGN KEY (`TypeID`) REFERENCES `Types` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Forms`
+--
+
+LOCK TABLES `Forms` WRITE;
+/*!40000 ALTER TABLE `Forms` DISABLE KEYS */;
+INSERT INTO `Forms` VALUES (1,'В каком примере выходит 4?',2,'1,2,3,4','1'),(2,'What the <b>correct answer at 30+28</b>',2,'5,6,7,8','6');
+/*!40000 ALTER TABLE `Forms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Questions`
+--
+
+DROP TABLE IF EXISTS `Questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Questions` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) DEFAULT NULL,
+  `EntityTo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fkEntityTo` (`EntityTo`),
+  CONSTRAINT `fkEntityTo` FOREIGN KEY (`EntityTo`) REFERENCES `Questions` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Questions`
+--
+
+LOCK TABLES `Questions` WRITE;
+/*!40000 ALTER TABLE `Questions` DISABLE KEYS */;
+INSERT INTO `Questions` VALUES (1,'2+2',NULL),(2,'4+10',NULL),(3,'8+10',NULL),(4,'2+16',NULL),(5,'10',NULL),(6,'58',NULL),(7,'43',NULL),(8,'102',NULL);
+/*!40000 ALTER TABLE `Questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Tests`
+--
+
+DROP TABLE IF EXISTS `Tests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Tests` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Forms` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Tests`
+--
+
+LOCK TABLES `Tests` WRITE;
+/*!40000 ALTER TABLE `Tests` DISABLE KEYS */;
+INSERT INTO `Tests` VALUES (1,'Simple math test','1,2');
+/*!40000 ALTER TABLE `Tests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Types`
+--
+
+DROP TABLE IF EXISTS `Types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Types` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Types`
+--
+
+LOCK TABLES `Types` WRITE;
+/*!40000 ALTER TABLE `Types` DISABLE KEYS */;
+INSERT INTO `Types` VALUES (1,'Checkboxes'),(2,'Radiobuttons'),(3,'QuestionAnswer');
+/*!40000 ALTER TABLE `Types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Login` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `Password` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-19  0:19:44
