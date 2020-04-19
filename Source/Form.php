dbad632c4ded7406087186dcc99e72ac9fac2934
Gamatoza TV <gamatext@yandex.ru>
2020-04-19 14:48:00 +0300

    added form chooser checkbox/radio/textarea, started answerGEN

--- Source/Form.php
+++ Source/Form.php
@@ -3,9 +3,9 @@ class Form
 {
     public $Questions = [];
     private $CorrectAnswer;
-    private $ID,$Name,$Type;
-    
-    public function __construct($ID = 0,$Name = NULL,$Type = 0)
+    private $ID, $Name, $Type;
+
+    public function __construct($ID = 0, $Name = NULL, $Type = 0)
     {
         $this->ID = $ID;
         $this->Name = $Name;
@@ -31,7 +31,7 @@ class Form
     {
         return $this->Type;
     }
-/*
+    /*
     public function getQuestionByID($id)
     {
         return $this->Questions[$id];
@@ -46,24 +46,46 @@ class Form
 */
 
 
-    /**
-     * Get the value of CorrectAnswer
-     */ 
     public function getCorrectAnswer()
     {
         return $this->CorrectAnswer;
     }
 
-    /**
-     * Set the value of CorrectAnswer
-     *
-     * @return  self
-     */ 
     public function setCorrectAnswer($CorrectAnswer)
     {
         $this->CorrectAnswer = $CorrectAnswer;
 
         return $this;
     }
+
+    public function drawForm($ID = 0)
+    {
+        echo '<div name="form'.$ID.'" id="'.$ID.'">';
+        //<!--Сюда вопрос -->
+        echo "<p><b>" . $this->getName() . "</b></br></p>";
+
+        switch ($this->Type) {
+            case 1:
+                $i = 0;
+                foreach ($this->Questions as $qo) {
+                    echo '<input type="checkbox" name="answerTo'.$ID.'[]" value="'.$i.'">' . $qo->getName() . '<Br>';
+                    $i+=1;
+                }
+                break;
+            case 2:
+                $i = 0;
+                foreach ($this->Questions as $qo) {
+                    echo '<input type="radio" name="answerTo'.$ID.'" value="'.$i.'">' . $qo->getName() . '<Br>';
+                    $i+=1;
+                }
+                break;
+            case 3:
+                echo '<textarea type="text" name="answerTo'.$ID.'"></textarea> <Br>';
+                break;
+            default:
+                # code...
+                break;
+        }
+        echo '</div>';
+    }
 }
-?>
--- testGenerator_old.php
+++ testGenerator_old.php
@@ -12,9 +12,6 @@ $FormsID = '(1,2,3)';
 
 
 $query = "SELECT * FROM Forms WHERE ID IN $FormsID"; //Можно юзать тот же шаблон только c WHERE, если нужен конкретный тест
-
-//mysqli_query($connect,"set names cp1251");
-
 $result = mysqli_query($connect, $query);
 $rows = mysqli_num_rows($result);
 
@@ -29,7 +26,7 @@ for ($i = 0; $i < $rows; $i++) {
     $testType = mysqli_fetch_row($Row);
 
     //Get Form
-    $Form = new Form($row[0], $row[1], $testType[0]);
+    $Form = new Form($row[0], $row[1], $testType[1]);
 
     //Get Questions
     $ExpSTR = '(' . $row[3] . ')';
@@ -57,17 +54,15 @@ mysqli_close($connect);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Document</title>
 </head>
 
 <style>
-    div {
-        width: 700px;
+    form {
+        width: auto;
         height: auto;
         /* Размеры */
         padding: 10px 10px 10px 10px;
-        margin: 10px 10px 10px 10px;
         background: #fc0;
         /* Цвет фона */
         outline: 2px solid #000;
@@ -80,17 +75,21 @@ mysqli_close($connect);
 </style>
 
 <body>
-    <form name="test" method="post" action="answerGenerator.php">
-        <?php
-        $i = 0;
-        foreach ($Test->Forms as $fm) {
-            $fm->drawForm($i);
-            $i++;
+    <?php
+    foreach ($Test->Forms as $fm) {
+        // Далее надо прописать класс Tests и уже массивом перекидывать, это чисто на старт-->
+        echo '<form name="test" method="post" action="input1.php">';
+        //<!--Сюда вопрос -->
+        echo "<p><b>" . $fm->getName() . "</b></br></p>";
+
+        foreach ($fm->Questions as $qo) {
+            echo '<input type="radio" name="answer" value="$j">' . $qo->getName() . '<Br>';
         }
-        ?>
-        <input type="submit" value="Закончить тест" />
-        <input type="reset" value="Очистить">
-    </form>
+        echo '<p><input type="submit" value="Отправить">';
+        echo '<input type="reset" value="Очистить"></p>';
+        echo '</form>';
+    }
+    ?>
 </body>
 
 </html>
